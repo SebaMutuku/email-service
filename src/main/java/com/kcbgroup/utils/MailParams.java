155127934c5811d85d10c9b60fe29bc6ad795df6
Sebastian Mutuku <KENCONT208173@kcbgroup.com>
2024-09-19 11:05:24 +0300

    Initial stages of build

---
+++ src/main/java/com/kcbgroup/utils/MailParams.java
@@ -0,0 +1,20 @@
+package com.kcbgroup.utils;
+
+import java.util.List;
+import lombok.Data;
+import lombok.ToString;
+
+
+@Data
+@ToString
+public class MailParams {
+    public String from;
+    public String to;
+    public String subject;
+    public String body;
+    public List<String> cc;
+    public String encodingPasscode;
+    public String fileContent;
+    public String fileName;
+
+}
