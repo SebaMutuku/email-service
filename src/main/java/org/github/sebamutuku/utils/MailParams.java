a913c26a78221d1ff9b523a90e613755598ecbf8
SebaMuutku <sebastian.muthiani@gmail.com>
2025-12-11 15:10:09 +0200

    Add javadocs

--- src/main/java/org/github/sebamutuku/utils/MailParams.java
+++ src/main/java/org/github/sebamutuku/utils/MailParams.java
@@ -1,20 +1,45 @@
 package org.github.sebamutuku.utils;
 
-import java.util.List;
 import lombok.Data;
 import lombok.ToString;
+
+import java.util.List;
 
 
 @Data
 @ToString
 public class MailParams {
+    /**
+     * Sender email address
+     */
     public String from;
+    /**
+     * Recipient email address
+     */
     public String to;
+    /**
+     * Email subject line
+     */
     public String subject;
+    /**
+     * Email message body content
+     */
     public String body;
+    /**
+     * CC recipients
+     */
     public List<String> cc;
+    /**
+     * Passcode for encoding/decoding
+     */
     public String encodingPasscode;
+    /**
+     * Attached file content (Base64 or raw)
+     */
     public String fileContent;
+    /**
+     * Name of the attached file
+     */
     public String fileName;
 
 }
